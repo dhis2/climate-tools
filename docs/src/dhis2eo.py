fe26a474242245b98b7249c39d1574f13336ee62
Bjørn Sandvik <bjorn@dhis2.org>
2025-09-14 11:32:09 +0200

    Reset change

--- docs/src/dhis2eo.py
+++ docs/src/dhis2eo.py
@@ -1,26 +1,23 @@
 import json
 from datetime import datetime
 
-def get_org_unit_from_array_item(item, org_unit_dim = 'orgUnit'):
-  return item.coords.get(org_unit_dim).data.item(0);  
+def getOrgUnitFromItem(item):
+  return item.coords.get('orgUnit').data.item(0);  
 
-# TODO: handle different period formats and types
-def get_period_from_array_item(item, period_dim = 'period'):
+def getPeriodFromItem(item):
   # 1e+9 converts from nanoseconds to seconds 
-  return datetime.fromtimestamp(item.coords.get(period_dim).item(0) / 1e+9).strftime('%Y-%m-%d')
+  return datetime.fromtimestamp(item.coords.get('period').item(0) / 1e+9).strftime('%Y-%m-%d')
 
-def get_value_from_array_item(item):
+def getValueFormItem(item):
   return item.data.item(0);
 
-# Translates an xarray.DataArray to JSON format used by DHIS2 Web API
-def data_array_to_dhis2_json(data_array, data_element_id, org_unit_dim, period_dim):
+# Translates an xarray.DataArray to the JSON format of the DHIS2 Web API
+def dataArrayToJson(dataArray):
   data = []
-  for item in data_array:     
+  for item in dataArray:     
     data.append({
-      'dataElement': data_element_id,
-      'orgUnit': get_org_unit_from_array_item(item, org_unit_dim),
-      'period': get_period_from_array_item(item, period_dim),  
-      'value': get_value_from_array_item(item),
+      'orgUnit': getOrgUnitFromItem(item),
+      'period': getPeriodFromItem(item),  
+      'value': getValueFormItem(item),
     })
-
-  return json.dumps({ "dataValues": data })
+  return json.dumps(data)
