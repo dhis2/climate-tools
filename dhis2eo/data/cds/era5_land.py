ea2434328b7302e2e0b876936ef01db678689e4c
Karim Bahgat <karim.bahgat.norway@gmail.com>
2025-11-13 20:37:18 +0000

    chore: improved netcdf cache decorator shared across datasets

--- dhis2eo/data/cds/era5_land.py
+++ dhis2eo/data/cds/era5_land.py
@@ -1,14 +1,12 @@
-import earthkit.data
-import geopandas as gpd
-import hashlib
 import json
-import os
-import tempfile
 import logging
 import calendar
-from datetime import date
-from pathlib import Path
 import sys
+
+import earthkit.data
+import xarray as xr
+
+from ..utils import netcdf_cache
 
 logger = logging.getLogger(__name__)
 
@@ -24,86 +22,26 @@ DEFAULT_VARIABLES = [
     'total_precipitation',
 ]
 
-# Try to fix cache issue by setting download threads to 1
+# Try to fix CDS cache issue by setting download threads to 1
 config = earthkit.data.config
 config.set('number-of-download-threads', 1)
 
-def generate_cache_filename(dataset, year, data_dict, month=None, bbox=None):
-    '''Generates a cache file name based on misc download parameters'''
-    stubs = []
-    # provider stub
-    provider_stub = 'cds'
-    stubs.append(provider_stub)
-    # dataset stub
-    stubs.append(dataset)
-    # data dict stub
-    data_dict_stub = 'params-' + hashlib.sha256(json.dumps(data_dict).encode('ascii')).hexdigest()[:6]
-    stubs.append(data_dict_stub)
-    # spatial stub if bbox is given
-    if bbox:
-        bbox_hash = hashlib.sha256(','.join(map(str, bbox)).encode('ascii')).hexdigest()[:6]
-        stubs.append('region-' + bbox_hash)
-    # time stub
-    time_stub = f'{year}'
-    if month:
-        time_stub += f'-{str(month).zfill(2)}'
-    stubs.append(time_stub)
-    # join the stubs to final filename without extension
-    filename = '_'.join(stubs)
-    return filename
-
-# TODO: old for era5 normal, redo for era5-land 
-def _get_daily_era5_data(year, month, org_units=None, bbox=None, data_dict=None, cache_folder=None):
-    '''Hardcoded cached version for daily era5 data'''
-    # TODO: make into generic decorator that can be applied to any download function
-    # ...needs to accept a dataset arg as well
-    current_date = date.today()
-    # get cache folder
-    cache_folder = tempfile.gettempdir()
-    # get or calculate bbox
-    if bbox is None:
-        if org_units is not None:
-            bbox = org_units.total_bounds.tolist()
-        else:
-            raise Exception('Either org_units or bbox have to be set')
-    # get default data dicts
-    data_dict = data_dict or DEFAULT_DAILY_ERA5_DATA_DICT
-    # convert input args to a cache filename
-    file_name = generate_cache_filename('daily-era5', year, data_dict=data_dict, month=month, bbox=bbox)
-    file_name += '.nc'
-    file_path = os.path.join(cache_folder, file_name)
-    # check if cache filename already exists
-    if os.path.exists(file_path):
-        # load from cache
-        logger.info(f'Loading from cache: {file_path}')
-        data = earthkit.data.from_source('file', file_path)
-    else:
-        # download data from the api
-        data = download_daily_era5_data(year=year, month=month, data_dict=data_dict, org_units=org_units)
-        # save to cache, but not if we're still in the current month
-        if year == current_date.year and month == current_date.month:
-            logger.info('Data is for the current month and will not be cached, since data is added daily')
-        else:
-            logger.info(f'Saving to cache: {file_path}')
-            data.to_target('file', file_path)
-            # Reload the data from the cache to ensure same .path attr
-            # Warning: a little hacky... 
-            data = earthkit.data.from_source('file', file_path)
-    # return
-    return data
-
 def get_hourly(year, month, org_units=None, bbox=None, variables=None, days=None):
     '''Download hourly era5-land data'''
+
     # get default variables
     variables = variables or DEFAULT_VARIABLES
+
     # get or calculate bbox
     if bbox is None:
         if org_units is not None:
             bbox = list(org_units.total_bounds)
         else:
             raise Exception('Either org_units or bbox have to be set')
+        
     # extract the coordinates from input bounding box
     xmin,ymin,xmax,ymax = bbox
+
     # construct the query parameters
     _,last_day = calendar.monthrange(year, month)
     days = days or [day for day in range(1, last_day+1)]
@@ -118,6 +56,7 @@ def get_hourly(year, month, org_units=None, bbox=None, variables=None, days=None
         "data_format": "netcdf",
         "download_format": "unarchived",
     }
+
     # download the data
     logger.info(f'Downloading data from CDS API...')
     logger.info(f'Request parameters: \n{json.dumps(params)}')
@@ -125,21 +64,29 @@ def get_hourly(year, month, org_units=None, bbox=None, variables=None, days=None
         "reanalysis-era5-land",
         **params
     )
+
+    # load lazily from disk using xarray
+    data_array = xr.open_dataset(data.path)
+
     # return
-    return data
+    return data_array
 
 def get_monthly(years, months, org_units=None, bbox=None, variables=None):
     '''Download monthly era5-land data'''
+
     # get default variables
     variables = variables or DEFAULT_VARIABLES
+
     # get or calculate bbox
     if bbox is None:
         if org_units is not None:
             bbox = list(org_units.total_bounds)
         else:
             raise Exception('Either org_units or bbox have to be set')
+    
     # extract the coordinates from input bounding box
     xmin,ymin,xmax,ymax = bbox
+
     # construct the query parameters
     params = {
         "product_type": ["monthly_averaged_reanalysis"],
@@ -151,6 +98,7 @@ def get_monthly(years, months, org_units=None, bbox=None, variables=None):
         "data_format": "netcdf",
         "download_format": "unarchived",
     }
+
     # download the data
     logger.info(f'Downloading data from CDS API...')
     logger.info(f'Request parameters: \n{json.dumps(params)}')
@@ -158,9 +106,14 @@ def get_monthly(years, months, org_units=None, bbox=None, variables=None):
         "reanalysis-era5-land-monthly-means",
         **params
     )
-    # return
-    return data
 
+    # load lazily from disk using xarray
+    data_array = xr.open_dataset(data.path)
+    
+    # return
+    return data_array
+
+@netcdf_cache()
 def get(period_type, **kwargs):
     '''Download era5-land data a period type'''
     if period_type == 'hourly':
